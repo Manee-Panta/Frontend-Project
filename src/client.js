97ea49eba9d6194852ed53733923a6ddded9fc2a
Maneepanta <72423886+Manee-Panta@users.noreply.github.com>
2022-11-19 22:14:37 +0545

    Update client.js

--- src/client.js
+++ src/client.js
@@ -1,8 +1,8 @@
-import {createClient} from 'contentful'
+// import {createClient} from 'contentful'
 
-export const client = createClient({
-    space:'elwxznc0zqn9',
-    accessToken:'llcW3UyuetnH4xFp7rVmIIGD4uom4T30XObKPwQuGiI'
-    // space:process.env.REACT_APP_SPACE_ID,
-    // accessToken:process.env.REACT_APP_ACCESS_TOKEN
-})
+// export const client = createClient({
+//     space:'elwxznc0zqn9',
+//     accessToken:'llcW3UyuetnH4xFp7rVmIIGD4uom4T30XObKPwQuGiI'
+//     // space:process.env.REACT_APP_SPACE_ID,
+//     // accessToken:process.env.REACT_APP_ACCESS_TOKEN
+// })
